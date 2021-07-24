3700d86f4fc21ae4471b7c5e658c78f3a6d207cb
Caphace-Ethan <ethanyoung135@gmail.com>
2021-07-24 21:51:59 +0300

    modified the file to run mlflow

--- scripts/app.py
+++ scripts/app.py
@@ -1,27 +1,17 @@
 import numpy as np
-import pandas as pd
-import streamlit as st
-import altair as alt
-import plotly.express as px
+from sklearn.linear_model import LogisticRegression
 
-st.set_page_config(page_title="Dashboard | Telecom User Data Analysis ", layout="wide")
+import mlflow
+import mlflow.sklearn
 
-def loadData():
-    pd.set_option('max_column', None)
-    loaded_data = pd.read_csv('./data/Resp_dataSet.csv')
-    return loaded_data
-
-def selectHandset():
-    df = loadData()
-    handset = st.multiselect("choose Device Type(s)", list(df['device_make'].unique()))
-    if handset:
-        df = df[np.isin(df, handset).any(axis=1)]
-        st.write(df)
-
-
-
-st.markdown("<h1 style='color:#0b4eab;font-size:36px;border-radius:10px;'>Dashboard | Telecommunication Users Data Analysis </h1>", unsafe_allow_html=True)
-selectHandset()
-# st.markdown("<p style='padding:10px; background-color:#000000;color:#00ECB9;font-size:16px;border-radius:10px;'>Section Break</p>", unsafe_allow_html=True)
-st.title("Data Visualizations")
-# with st.beta_expander("Show More Graphs"):
+if __name__ == "__main__":
+    mlflow.set_experiment(experiment_name='TestExperiment')
+    X = np.array([-2, -1, 0, 1, 2, 1]).reshape(-1, 1)
+    y = np.array([0, 0, 1, 1, 1, 0])
+    lr = LogisticRegression()
+    lr.fit(X, y)
+    score = lr.score(X, y)
+    print("Score: %s" % score)
+    mlflow.log_metric("score", score)
+    mlflow.sklearn.log_model(lr, "model")
+    print("Model saved in run %s" % mlflow.active_run().info.run_uuid)
